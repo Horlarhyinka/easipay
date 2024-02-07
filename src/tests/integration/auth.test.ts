094a6d7acebecaf0c9e43bf4b7b665a90c81f566
Horlarhyinka <danijufarouq2003@gmail.com>
2024-02-07 03:26:22 +0100

    refactored auth test

--- src/tests/integration/auth.test.ts
+++ src/tests/integration/auth.test.ts
@@ -21,67 +21,67 @@ describe("register", ()=>{
         return request(server).post("/api/v1/auth/register").send(body)
     }
 
+    const obj = {email: "testing@gmail.com", password: "testing", firstName: "test", lastName: "test", tel: "123456789"}
+
     it("should return status 400 if no email or password is provided",async()=>{
         const res = await exec({})
         expect(res.statusCode!).toBe(400)
     })
     it("should return status 400 if invalid email is provided",async()=>{
-        const res = await exec({email: "test", password: "testing"})
+        const res = await exec({...obj, email: "invalid"})
         expect(res.statusCode!).toBe(400)
     })
     it("should return status 400 if firstName is not provided", async()=>{
-        const res = await exec({email: "testing@gmail.com", password: "testing", lastName: "test"})
+        const res = await exec({...obj, firstName: undefined})
         expect(res.statusCode!).toBe(400)
     })
     it("should return status 400 if lastName is not provided", async()=>{
-        const res = await exec({email: "testing@gmail.com", password: "testing", firstName: "test"})
+        const res = await exec({...obj, lastName: undefined})
         expect(res.statusCode!).toBe(400)
     })
     it("should return status 400 if tel is not provided", async()=>{
-        const res = await exec({email: "testing@gmail.com", password: "testing", lastName: "test"})
+        const res = await exec({...obj, tel: undefined})
         expect(res.statusCode!).toBe(400)
     })
     it("should return status 400 if password is less than 6",async()=>{
-        const res = await exec({email: "testing@gmail.com", password: "test", firstName: "test", lastName: "test", tel: "123456789"})
+        const res = await exec({...obj, password: "test"})
         expect(res.statusCode!).toBe(400)
     })
     it("should return 409 if email is already registered", async()=>{
-        const obj = {email: "testing@gmail.com", password: "testing", firstName: "test", lastName: "test", tel: "123456789"}
         await User.create(obj)
         const res = await exec(obj)
         expect(res.statusCode!).toBe(409)
     })
     it("should return 200 status code for valid informations",async()=>{
 
-        const res = await exec({email: "testing@gmail.com", password: "testing", firstName: "test", lastName: "test", tel: "123456789"})
+        const res = await exec(obj)
         expect(res.statusCode!).toBe(200)
     })
 })
 
 describe("login", ()=>{
-    const createTestUser = async()=>User.create({email: "testing@gmail.com", password: "testing", firstName: "test", lastName: "test", tel: "123456789"})
+    const obj = {email: "testing@gmail.com", password: "testing", firstName: "test", lastName: "test", tel: "123456789"}
+    const createTestUser = async()=>User.create(obj)
     it("should return status 400 code if email or password is not provided",async()=>{
         const res = await request(server).post("/api/v1/auth/login").send({})
-
         expect(res.statusCode!).toBe(400)
 
     })
     it("should return 404 if email is not registered", async()=>{
-        const res = await request(server).post("/api/v1/auth/login").send({email: "notfound@gmail.com",password: "testing"})
+        const res = await request(server).post("/api/v1/auth/login").send({...obj, email: "notfound@gmail.com"})
         expect(res.statusCode!).toBe(404)
     })
     it("should return 400 if password is incorrect", async()=>{
         await createTestUser()
-        const res = await request(server).post("/api/v1/auth/login").send({email: "testing@gmail.com",password: "test"})
+        const res = await request(server).post("/api/v1/auth/login").send({...obj, password: "test"})
         expect(res.statusCode!).toBe(400)
         await flushDb()
     })
     it("should return 200 status code for valid credentials",async()=>{
         await createTestUser()
-        const res = await request(server).post("/api/v1/auth/login").send({email: "testing@gmail.com",password: "testing"})
+        const res = await request(server).post("/api/v1/auth/login").send({email: obj.email, password: obj.password})
         expect(res.statusCode!).toBe(200)
         await flushDb()
     })
-    console.log("login test suite ran...")
 })
 
